8b5667a2fcf5672685be19e9d62ccc45c821025e
Josh Muncke <joshxthomas@gmail.com>
2019-02-24 11:16:56 -0800

    Check domain and client key in set token

--- R/token.R
+++ R/token.R
@@ -11,6 +11,18 @@
 #' @param password user's Egnyte password
 #' @export
 set_token <- function(domain, app_key, username, password) {
+  # Check domain
+  if(!validate_domain(domain)) {
+    stop("Invalid Egnyte domain.",
+         call. = FALSE)
+  }
+
+  # Check client id
+  if(stringr::str_length(app_key) != 24) {
+    stop("Egnyte application key does not look valid. Should be of the form: cba97f3apst9eqzdr5hskggx",
+         call. = FALSE)
+  }
+
   token_value <- authenticate_user(domain, app_key, username, password)
 
   options(egnyter.auth_token = token_value)
