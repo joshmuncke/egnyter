2c1858f696b86e63f16d4d8301d8a278b6b85079
Josh Muncke <joshxthomas@gmail.com>
2019-02-24 10:12:33 -0800

    Import magrittr pipe and documentation of utils

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,13 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+# Validation of egnyte filesystem domains
 validate_domain <- function(domain) {
   stringr::str_detect(domain, 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)')
 }
 
+# Validate format of authorisation/bearer token
 validate_token <- function(token) {
   stringr::str_detect(token, 'Bearer [a-z0-9]{24}')
 }
